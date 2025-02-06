c0e0d983cf874a9c2899692aeeb4568f89576886
Krishna3558 <rajeevmishra6066@gmail.com>
2025-02-06 22:00:30 +0530

    Update Signup.jsx

--- frontend/foodApp/src/screen/Signup.jsx
+++ frontend/foodApp/src/screen/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
     const [message , setMessage] = useState('');
     const [loading , setLoading] = useState(false);
     const [error , setError] = useState({});
-    const url = "http://localhost:3000";
+    const url = "https://foodapp-omih.onrender.com";
 
     const navigate = useNavigate();
 
